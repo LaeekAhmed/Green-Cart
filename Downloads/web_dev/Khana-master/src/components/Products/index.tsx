e4dca51373fe5402d8d608aad26212adada8d056
Huzaifa Khan <huzaifakhan@Huzaifas-MacBook-Air.local>
2023-01-14 21:07:13 -0500

    products

--- Downloads/web_dev/Khana-master/src/components/Products/index.tsx
+++ Downloads/web_dev/Khana-master/src/components/Products/index.tsx
@@ -1,9 +1,111 @@
+import "./Products.scss";
+import eggs from "./Eggs.jpg";
+
 function Products() {
   return (
     <div>
-      <h1>
-        Welcome!
-      </h1>
+        <div className="title" >
+        <h1> Local Produce</h1>
+        </div>
+        <div className="products">
+            <div className="product">
+                <img src = {eggs} alt="Eggs" />
+                <div className="heading"> <p> Eggs </p > <p> $2.00 </p></div>
+                
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div> 
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+            <div className="product">
+            <img src = {eggs} alt="Eggs" />
+                <h3> Eggs </h3>
+                <p> $2.00 </p>
+                <p> 1 dozen </p>
+                <p>Farmer: Billy</p>
+                <button> Add to Cart </button>
+            </div>
+        </div>
+
     </div>
   );
 }
