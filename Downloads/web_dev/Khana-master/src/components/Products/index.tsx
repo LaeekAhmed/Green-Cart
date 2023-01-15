0ba9c514902f53dcae71e57eb74f83264aaf238a
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2023-01-14 21:46:50 -0500

    produce pics + pricing

--- Downloads/web_dev/Khana-master/src/components/Products/index.tsx
+++ Downloads/web_dev/Khana-master/src/components/Products/index.tsx
@@ -1,5 +1,17 @@
 import "./Products.scss";
 import eggs from "./Eggs.jpg";
+import gchicken from "./gchicken.jpg"
+import bchicken from "./bchicken.jpg"
+import tchicken from "./tchicken.jpg"
+import wchicken from "./wchicken.jpg"
+import potatoes from "./potatoes.jpg"
+import radish from "./radish.jpg"
+import rasp from "./raspberry.jpg"
+import straw from "./straw.jpg"
+import leg from "./legumes.jpg"
+import blackberry from "./blackberry.jpg"
+import corn from "./corn.jpg"
+
 
 function Products() {
   return (
@@ -10,98 +22,98 @@ function Products() {
         <div className="products">
             <div className="product">
                 <img src = {eggs} alt="Eggs" />
-                <div className="heading"> <p> Eggs </p > <p> $2.00 </p></div>
+                <div className="heading"> <p> Eggs </p > <p> $4.25 </p></div>
                 
                 <p> 1 dozen </p>
                 <p>Farmer: Billy</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {gchicken} alt="Eggs" />
+                <h3> Ground Chicken </h3>
+                <p> $8.95 </p>
+                <p> 1 lb </p>
+                <p>Farmer: Bob</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {bchicken} alt="chicken breast" />
+                <h3> Chicken Breast </h3>
+                <p> $3.725 </p>
+                <p> 1 lb</p>
+                <p>Farmer: Bob</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {tchicken} alt="Eggs" />
+                <h3>Chicken Thighs</h3>
+                <p> $2.40</p>
+                <p> 1 lb</p>
+                <p>Farmer: Bob</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {wchicken} alt="Eggs" />
+                <h3> Chicken Wings </h3>
+                <p> $2.68 </p>
+                <p> 1 lb </p>
+                <p>Farmer: Bob</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {potatoes} alt="Eggs" />
+                <h3> Potatoes </h3>
+                <p> $2.01 </p>
+                <p> 1 lb </p>
+                <p>Farmer: Amber</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {leg} alt="Eggs" />
+                <h3> Legumes</h3>
+                <p> $0.51 </p>
+                <p> 1 lb </p>
+                <p>Farmer: Amber</p>
                 <button> Add to Cart </button>
             </div> 
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {corn} alt="Eggs" />
+                <h3> Corn </h3>
+                <p> $0.68</p>
+                <p> 1 lb </p>
+                <p>Farmer: Joe</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {radish} alt="Eggs" />
+                <h3> Radish </h3>
+                <p> $2.67 </p>
+                <p> 1 lb </p>
+                <p>Farmer: Joe</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {blackberry} alt="Eggs" />
+                <h3> Blackberries </h3>
+                <p> $5.66 </p>
+                <p> 1 lb </p>
+                <p>Farmer: Jake</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {straw} alt="Eggs" />
+                <h3> Strawberries </h3>
+                <p> $2.22</p>
+                <p> 1 lb </p>
+                <p>Farmer: Jake</p>
                 <button> Add to Cart </button>
             </div>
             <div className="product">
-            <img src = {eggs} alt="Eggs" />
-                <h3> Eggs </h3>
-                <p> $2.00 </p>
-                <p> 1 dozen </p>
-                <p>Farmer: Billy</p>
+            <img src = {rasp} alt="Eggs" />
+                <h3> Raspberries </h3>
+                <p> $6.88 </p>
+                <p> 1 lb </p>
+                <p>Farmer: Jake</p>
                 <button> Add to Cart </button>
             </div>
         </div>
--- Downloads/web_dev/Khana-master/src/components/Products/productsDB.ts
+++ Downloads/web_dev/Khana-master/src/components/Products/productsDB.ts
@@ -11,39 +11,39 @@ export const Options: ProductOptions = [
         price: "$4.99/dozen",
         quantity:"0",
         farmer:"Billy",
-        description:"eggs by the dozen",
+        description:"Organic large freerun eggs by the dozen",
 
     },
 
     {
         id:2,
         // product name
-        product_name: "",
-        price: "$4.99/dozen",
+        product_name: "ground chicken",
+        price: "$8.95/pound",
         quantity:"0",
-        farmer:"Billy",
-        description:"eggs by the dozen",
+        farmer:"Bob",
+        description:"boneless skinless ground chicken by the pound",
 
     },
 
     {
         id:3,
         // product name
-        product_name: "eggs",
-        price: "$4.99/dozen",
+        product_name: "chicken thighs",
+        price: "$5.40/pound",
         quantity:"0",
-        farmer:"Billy",
-        description:"eggs by the dozen",
+        farmer:"Amber",
+        description:"boneless skinless chicken thighs by the pound",
 
     },
 
     {
         id:4,
         // product name
-        product_name: "eggs",
-        price: "$4.99/dozen",
+        product_name: "whole chicken",
+        price: "$4.75/pound",
         quantity:"0",
-        farmer:"Billy",
+        farmer:"Amber",
         description:"eggs by the dozen",
 
     },
@@ -51,8 +51,8 @@ export const Options: ProductOptions = [
     {
         id:5,
         // product name
-        product_name: "eggs",
-        price: "$4.99/dozen",
+        product_name: "lettuce",
+        price: "$8/dozen",
         quantity:"0",
         farmer:"Billy",
         description:"eggs by the dozen",
