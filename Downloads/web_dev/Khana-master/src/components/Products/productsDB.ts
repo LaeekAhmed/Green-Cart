0ca6d3c1f491de3accae70632a6590f0829ea3db
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2023-01-14 19:49:40 -0500

    products

---
+++ Downloads/web_dev/Khana-master/src/components/Products/productsDB.ts
@@ -0,0 +1,143 @@
+// import { ProductOptions ,OptionType1} from "../../types/Types1";
+
+
+// export const Options: ProductOptions = [
+//         // farmer name, then list of products
+//         //ideally we would identify a list of farmers near the user (e.g. 5km)
+//         //and put the names of the farmers in here
+
+//     {
+//         id:1,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:2,
+//         // product name
+//         product_name: "",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:3,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:4,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:5,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:6,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:7,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:8,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:9,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:10,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:11,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     },
+
+//     {
+//         id:12,
+//         // product name
+//         product_name: "eggs",
+//         price: "$4.99/dozen",
+//         quantity:"0",
+//         farmer:"Billy",
+//         description:"eggs by the dozen",
+
+//     }
+
+// ]
+
+export{}
--- Downloads/web_dev/Khana-master/src/types/Types.ts
+++ Downloads/web_dev/Khana-master/src/types/Types.ts
@@ -2,8 +2,11 @@ export type OptionType = {
     header: string;
     offerings: string[];
     price: number,
+    // ? indicates unknown value decided by
     count?: number;
     deliveryDate?: Date;
 };
 
 export type CaterorOptions = { [arg: string]: OptionType[] };
+
+
